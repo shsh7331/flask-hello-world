2636c16e2e7a43e4c32833478340b43c97261698
Shrey Shah <shsh7331@colorado.edu>
2025-03-26 03:14:31 +0000

    Updated select route - response string closing bracket
    Updated select route - response string closing bracket

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def selecting():
     for player in records:
         response_string+="<tr>"
         for info in player:
-            response_string+="<td>{}</td}".format(info)
+            response_string+="<td>{}</td>}".format(info)
         resonse_string+="</tr>"
     response_string+="</tables>"
     return response_string
